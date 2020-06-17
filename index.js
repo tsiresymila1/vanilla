c39986351900fa6b128ec4474b3dff56f53388c1
tsiresy <tsiresymila@gmail.com>
2020-06-17 11:26:34 +0200

    Add vanilla pay local

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-var Payment = require('vanillapay');
+var Payment = require('./vanillapay');
 var express = require('express');
 var path = require('path');
 var engine = require('ejs-locals');
 var bodyParser = require('body-parser');
-// get Paiment instance.
 //TsiresyMila(AT534)
-var public_key = "d575a554eb6ff6cad76d4ba89c74ab69b477427e7ef9334eb3";
-var private_key = "b8e4f2141e64a79bf5724456cb2181ab08294f1a10cb6e8b71";
-
-var public_key1 = "e06dc3eed370eacd5ec554bbd2ea4f9bfb4cae4c22836c91dc"
-var private_key1 = "7911240462573087c6da0678f8ee7f58607cd5437f453ce3fb"
+var public_key = "6a10ca0036e408a2306bbb26788be3f6ddc759abe5a19692ef";
+var private_key = "e7f3b743de4374128c7076813d2ee36b16e67720c6fe4018b2";
 
 var client_id = "179_2kt85b5gybc440o48o880go4g08g4k4oo8kkkooos08cs4o0sk";
 var client_secret = "pia5gn4xsis0ow8sgg0ss84wc88sog8sg0scw0sksocs84o4c";
-
-var payment = new Payment("https://vanillashop.herokuapp.com", client_id, client_secret, public_key, private_key);
+// get Paiment instance.
+var payment = new Payment("https://vanillapay.glitch.me", client_id, client_secret, public_key, private_key);
 
 var app = express();
 // set the view engine to ejs
@@ -34,19 +30,11 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
     console.log(req.body.montant)
-    payment.initPaie("1", req.body.montant, "Tsiresy Mila", "RP 235230", "172.26.36.26", function (idpayment) {
+    payment.initPaie("1", req.body.montant, "Digi-Talent", "RP 235230", "52.22.30.237", function (idpayment) {
         console.log(idpayment);
         res.render('pages/index');
-    },function(error){
-        payment = new Payment("https://vanillashop.herokuapp.com", client_id, client_secret, public_key1, private_key1);
-        payment.initPaie("1", req.body.montant, "Tsiresy Mila", "RP 235230", "172.26.11.153", function (idpayment) {
-            console.log(idpayment);
-            res.render('pages/index');
-        },function(error){
-            //payment = new Payment("https://vanillashop.herokuapp.com", client_id, client_secret, public_key, private_key);
-            res.render('pages/error', { data: error });
-        });
-        //res.render('pages/error', { data: error });
+    }, function (error) {
+        res.render('pages/error', { data: error });
     });
 });
 
